c13991cee993cef68d050b861c3fc9687cc04fd3
miaplesyrup <delmai0721@gmail.com>
2024-05-05 13:35:03 +0800

    modified some css

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -4,23 +4,23 @@
 
 <body>
   <article>
-    <h1><a href="/posts/my-first-post">Grateful</a></h1>
+    <h1><a href="/posts/my-first-post">Hello</a></h1>
     <p>
-        It's been three weeks of intense work. I don't know when it will finish. It's a mini-roller coaster ride. But those little moments make up for the bad times.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer leo massa, interdum nec malesuada elementum, eleifend vel velit. Praesent dui elit, vestibulum eget quam sed, mollis congue felis. Nam quis risus dolor. Nullam viverra enim in leo semper, nec laoreet quam ullamcorper. Nullam elementum neque neque, ac eleifend neque viverra non. Nunc a sem mattis, rutrum lorem quis, viverra mi. Donec leo sem, convallis sit amet sollicitudin ultrices, porta eget quam.
     </p>
   </article>
 
   <article>
-    <h1><a href="/posts/my-second-post">Praying</a></h1>
+    <h1><a href="/posts/my-second-post">Post2</a></h1>
     <p>
-        It's been three weeks of intense work. I don't know when it will finish. It's a mini-roller coaster ride. But those little moments make up for the bad times.
+        Vivamus in vulputate ex, et eleifend dolor. Curabitur non luctus nisi, at lobortis tellus. Aenean vel molestie ex, vitae mollis leo. Nunc dignissim tincidunt elementum. Aenean ut dui vitae sem hendrerit auctor condimentum eu purus. Morbi cursus ex eu interdum consequat. Phasellus nec ex eros. Suspendisse pharetra ut ex quis accumsan. Nunc finibus metus vitae ipsum placerat, a rhoncus urna mattis. Vestibulum quis congue enim, vitae fringilla nulla. Donec imperdiet commodo vehicula. Praesent suscipit libero quis felis consectetur, quis viverra augue bibendum.
     </p>
   </article>
 
   <article>
-    <h1><a href="/posts/my-third-post">Grateful</a></h1>
+    <h1><a href="/posts/my-third-post">Post3</a></h1>
     <p>
-        It's been three weeks of intense work. I don't know when it will finish. It's a mini-roller coaster ride. But those little moments make up for the bad times.
+        Phasellus vel egestas turpis, sed elementum odio. Vivamus non lectus nec urna fringilla rhoncus. Duis vel nulla maximus, hendrerit sem non, accumsan velit. Donec nulla enim, aliquam at justo id, tincidunt vulputate tellus. Fusce in vulputate arcu. Morbi aliquet, diam lobortis faucibus sollicitudin, augue ex finibus erat, ac placerat sem dolor at sem. In vulputate eleifend tellus, eget dapibus libero malesuada in. In placerat enim ut scelerisque congue. Donec et malesuada lacus. Nam lobortis massa congue massa auctor volutpat.
     </p>
   </article>
 
